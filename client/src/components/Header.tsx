e5add54d5b9b7cc18bf01738220049c6d4848477
Ronny2k-git <ronnyedson12@gmail.com>
2026-04-12 17:02:32 -0300

    wip: create header and footer components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="w-full h-20 flex items-center gap-4 justify-center border-b-5 border-solid border-cyan-500">
+      <img src={"/favicon.png"} className="size-12" />
+      <div>dwdw</div>
+    </div>
+  );
+}
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,32 +1,3 @@
-import { useState } from "react";
-import heroImg from "../img/background-large.jpg";
-import reactLogo from "../img/background-medium.jpg";
-import viteLogo from "../img/background.jpg";
-
 export default function Home() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <main className="w-full h-full ">
-      <section id="center">
-        <div className="h-full ">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-    </main>
-  );
+  return <main className="w-full h-full "></main>;
 }
